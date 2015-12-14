211269f9bc286bf5999969c7a08d68a9fcff344a
Alberto Massa <alberto.massa@linkme.it>
2015-12-14 12:45:33 +0100

    added warning

--- sender.js
+++ sender.js
@@ -8,7 +8,13 @@ const SERVER = 'http://mean.link-me.it:3000';
 
 module.exports.send = function(name) {
 
-  var username = fs.readFileSync(USERNAME_FILE, 'utf8');
+  try {
+    var username = fs.readFileSync(USERNAME_FILE, 'utf8');
+  } catch(e) {
+    console.log('\n\n\n----------------------------------------------------------');
+    console.log('ATTENTION: YOU SHOULD PASS THE EXCERCISE 0 BEFORE CONTINUE\n\n\n');
+    process.exit();
+  }
 
   var postData = {
     username: username,
