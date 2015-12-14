9ea519de8c66ecad20d4c1ba698d6bb33d58724b
Alberto Massa <alberto.massa@linkme.it>
2015-12-14 18:49:23 +0100

    fixed typo

--- problems/0_myname/index.js
+++ problems/0_myname/index.js
@@ -10,8 +10,8 @@ exports.verify = verify({ modeReset: true, name:'ex-0' }, function (args, t) {
   var f = require(path.resolve(args[0]));
 
   t.equal(typeof f, 'object', 'does not export a JSON object' );
-  t.equal(f.hasOwnProperty('username'), true, 'does not export a JSON with name ' );
-  t.equal(typeof f.username, 'string', 'does not export string name ' );
+  t.equal(f.hasOwnProperty('username'), true, 'does not export a JSON with username field ' );
+  t.equal(typeof f.username, 'string', 'does not export string username ' );
 
   fs.writeFileSync(sender.USERNAME_FILE, f.username);
 
